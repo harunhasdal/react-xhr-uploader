3f37cf966f774c0fcb6fd368989c91549c74ecd0
Alexander <akaSStalkALEX@gmail.com>
2017-09-29 10:36:31 +0500

    update react css transition lib
    drop react-addons-css-transition-group in favour of react-transition-group as it was deprecated and dropped in last React 16.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import ReactCSSTransitionGroup from 'react-transition-group';
 import defaultStyles from './styles';
 
 class XHRUploader extends Component {
